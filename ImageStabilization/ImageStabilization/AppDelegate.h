11991b11edb349fa4be49a44a01d6da8e79b838e
EunchulJeon <eunchul.jeon@navercorp.com>
2015-09-12 12:38:10 +0900

    First commit

---
+++ ImageStabilization/ImageStabilization/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageStabilization
+//
+//  Created by EunchulJeon on 2015. 9. 12..
+//  Copyright (c) 2015년 EunchulJeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageStabilization/ImageStabilization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageStabilization
+//
+//  Created by EunchulJeon on 2015. 9. 12..
+//  Copyright (c) 2015년 EunchulJeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
